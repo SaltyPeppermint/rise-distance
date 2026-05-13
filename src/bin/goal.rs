4a422c36ab1b7b85cf7bff5667a8f1c9f568dafb
Nicole Heinimann <heinimann@tu-berlin.de>
2026-05-13 16:57:11 +0200

    custom allocators give us a tiny bit

--- src/bin/goal.rs
+++ src/bin/goal.rs
@@ -1,3 +1,6 @@
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 use std::env::current_dir;
 use std::fs::File;
 use std::io::BufWriter;
--- src/bin/guide.rs
+++ src/bin/guide.rs
@@ -1,3 +1,6 @@
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::BufWriter;
